8d9df51b5e6f5db16394fca2914eea9d35662a49
manoj-17-29 <manoj1729official@gmail.com>
2024-07-24 14:03:58 +0530

    final change

--- src/App.tsx
+++ src/App.tsx
@@ -1,135 +1,154 @@
-import { useState } from 'react'
-import WeatherDetails from './WeatherDetails';
+import { useState } from "react";
+import WeatherDetails from "./WeatherDetails";
 import { FaSearch } from "react-icons/fa";
 
-
-
 function App() {
+  const api = import.meta.env.VITE_api_key;
 
-const api = import.meta.env.VITE_api_key;
+  //const [icon,setIcon] = useState(profile);
+  const [temp, setTemp] = useState("0");
+  const [maxtemp, setMaxTemp] = useState("0");
+  const [mintemp, setMinTemp] = useState("0");
+  const [city, setCity] = useState("City");
+  const [country, setCountry] = useState("Country");
+  const [weather, setWeather] = useState("weather");
+  const [weatherdes, setWeatherDes] = useState("description");
+  const [lat, setLat] = useState("0");
+  const [lon, setLon] = useState("0");
+  const [humidity, setHumidity] = useState("0");
+  const [pressure, setPressure] = useState("0");
+  const [sealevel, setSeaLevel] = useState("0");
+  const [grndlevel, setGrdLevel] = useState("0");
+  const [wind, setWind] = useState("0");
+  const [text, setText] = useState("");
+  const [err, setErr] = useState("");
+  const [errt, setErrt] = useState(false);
+  const [cityNotFound, setCityNotFound] = useState(false);
+  const [loading, setLoading] = useState(false);
 
+  const search = async () => {
+    setLoading(true);
 
-//const [icon,setIcon] = useState(profile);
-const [temp, setTemp] = useState("0");
-const [city, setCity] = useState("City");
-const [country, setCountry] = useState("Country");
-const [lat, setLat] = useState("0");
-const [lon, setLon] = useState("0");
-const [humidity, setHumidity] = useState("0");
-const [wind, setWind] = useState("0");
-const [text, setText] = useState("");
-const [err, setErr] = useState("");
-const [errt, setErrt] = useState(false);
-const [cityNotFound, setCityNotFound] = useState(false);
-const [loading, setLoading] = useState(false);
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${api}&units=Metric`;
 
-const search = async () => {
-  setLoading(true);
+    try {
+      let res = await fetch(url);
+      let data = await res.json();
+      // console.log(data);
+      console.log(data);
 
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${api}&units=Metric`;
+      if (data.cod === "404") {
+        console.log("city not found");
+        setCityNotFound(true);
+        setLoading(false);
+        setErrt(false);
+      }
 
-  try {
-    let res = await fetch(url);
-    let data = await res.json();
-    // console.log(data);
-        console.log(api);
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setTemp(Math.floor(data.main.temp).toString());
+      setMaxTemp(Math.floor(data.main.temp_max).toString());
+      setMinTemp(Math.floor(data.main.temp_min).toString());
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setLat(data.coord.lat);
+      setLon(data.coord.lon);
+      setCityNotFound(false);
+      setWeather(data.weather[0].main);
+      setWeatherDes(data.weather[0].description);
+      setPressure(data.main.pressure);
+      setSeaLevel(data.main.sea_level);
+      setGrdLevel(data.main.grnd_level);
 
-
-    if (data.cod === "404") {
-      console.log("city not found");
-      setCityNotFound(true);
+      setErrt(false);
+    } catch (error: any) {
+      console.log("Error:", console.error);
+      setErr("An Error occured while fetching");
+      setErrt(true);
+    } finally {
       setLoading(false);
       setErrt(false);
     }
+  };
 
-    setHumidity(data.main.humidity);
-    setWind(data.wind.speed);
-    setTemp(Math.floor(data.main.temp).toString());
-    setCity(data.name);
-    setCountry(data.sys.country);
-    setLat(data.coord.lat);
-    setLon(data.coord.lon);
-    setCityNotFound(false);
-    setErrt(false);
-  } catch (error : any) {
-    console.log("Error:", console.error);
-    setErr("An Error occured while fetching");
-    setErrt(true);
-  } finally {
-    setLoading(false);
-    setErrt(false);
+  const handlecity = (e: any) => {
+    setText(e.target.value);
+  };
+
+  function handlekeydown(e: any) {
+    if (e.key === "Enter") {
+      search();
+    }
   }
-};
 
-const handlecity = (e :any) => {
-  setText(e.target.value);
-};
+  return (
+    <>
+      <div className="flex justify-center items-center h-dvh">
+        <div
+          id="style-7"
+          className="w-11/12 md:w-8/12 lg:w-5/12 bg-blue-100 rounded-xl px-3 py-4 h-[85vh] lg:h-[90vh] overflow-y-auto"
+        >
+          <div className="">
+            <h1 className="text-blue-700 text-2xl font-bold text-start my-3 ms-2 tracking-wide">
+              Weather Finder :
+            </h1>
 
-function handlekeydown(e : any) {
-  if (e.key === "Enter") {
-    search();
-  }
-}
-
-return (
-  <>
-    <div className="prd-bg">
-      <div className=" py-6 block">
-        <h1 className="text-blue-700 text-2xl lg:text-4xl font-bold text-center ">
-          Weather Finder
-        </h1>
-      </div>
-      <div className="flex container  text-center py-2 justify-center ">
-        <div className="input-container bg-blue-100 w-full md:w-8/12 lg:w-5/12 p-2 md:p-5 py-10 mx-5 mt-2 rounded-2xl">
-          <div className="bg-white rounded-lg py-2 w-full  px-2 ps-3 flex items-center border-2 ">
-            <input
-              type="text"
-              className="border-none outline-none w-full text-blue-600"
-              onChange={handlecity}
-              value={text}
-              onKeyDown={handlekeydown}
-              placeholder="Enter a city name"
-            />
-            <div className="bg-slate-200 px-2 py-1 rounded-lg flex justify-center items-center gap-2">
-              <FaSearch onClick={() => search()} />
-              <button onClick={() => search()}>search</button>
+            <div className="bg-white rounded-lg py-2 w-full  px-2 ps-3 flex items-center border-2 ">
+              <input
+                type="text"
+                className="border-none outline-none w-full text-blue-600"
+                onChange={handlecity}
+                value={text}
+                onKeyDown={handlekeydown}
+                placeholder="Enter a city name"
+              />
+              <div className="bg-slate-200 px-2 py-1 rounded-lg flex justify-center items-center gap-2">
+                <FaSearch onClick={() => search()} />
+                <button onClick={() => search()}>search</button>
+              </div>
             </div>
+
+            {loading && (
+              <p className="py-10 text-center my-12 font-medium text-lg">
+                Fetching Data ...{" "}
+              </p>
+            )}
+
+            {cityNotFound && !loading && (
+              <p className="py-10 text-center my-12  text-xl text-red-600 font-medium">
+                City not found . . .
+              </p>
+            )}
+
+            {errt && !loading && (
+              <p className="py-10 text-center my-12  text-xl text-red-600 font-medium">
+                {err}
+              </p>
+            )}
+
+            {!cityNotFound && !loading && !errt && (
+              <WeatherDetails
+                temp={temp}
+                city={city}
+                country={country}
+                lat={lat}
+                lon={lon}
+                humidity={humidity}
+                wind={wind}
+                weather={weather}
+                weatherdes={weatherdes}
+                maxtemp={maxtemp}
+                mintemp={mintemp}
+                pressure={pressure}
+                sealevel={sealevel}
+                grndlevel={grndlevel}
+              />
+            )}
           </div>
-
-          {loading && (
-            <p className="py-10 font-medium text-lg">Fetching Data ... </p>
-          )}
-
-          {cityNotFound && !loading && (
-            <p className="py-10 text-xl text-red-600 font-medium">
-              City not found . . .
-            </p>
-          )}
-
-          {errt && !loading && (
-            <p className="py-10 text-xl text-red-600 font-medium">{err}</p>
-          )}
-
-          {!cityNotFound && !loading && !errt && (
-            <WeatherDetails
-              temp={temp}
-              city={city}
-              country={country}
-              lat={lat}
-              lon={lon}
-              humidity={humidity}
-              wind={wind}
-            />
-          )}
         </div>
       </div>
-    </div>
-  </>
-);
-
-
-
-  
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/WeatherDetails.tsx
+++ src/WeatherDetails.tsx
@@ -1,4 +1,4 @@
-import { IoWater } from "react-icons/io5";
+import { MdOutlineWaterDrop } from "react-icons/md";
 import { WiCloudyWindy } from "react-icons/wi";
 
 
@@ -10,6 +10,13 @@ interface WeatherDetailsProps {
   lon: any;
   humidity: any;
   wind: any;
+  weatherdes: any;
+  weather: any;
+  maxtemp: any;
+  mintemp: any;
+  pressure: any;
+  sealevel: any;
+  grndlevel: any;
 }
 
 const WeatherDetails: React.FC<WeatherDetailsProps> = ({
@@ -20,42 +27,93 @@ const WeatherDetails: React.FC<WeatherDetailsProps> = ({
   lon,
   humidity,
   wind,
+  weatherdes,
+  weather,
+  maxtemp,
+  mintemp,
+  pressure,
+  sealevel,
+  grndlevel,
 }) => {
   return (
-    <div className="py-10">
+    <div className="py-6">
       <img
         src="/weather.jpg"
         alt=""
-        className="w-6/12 md:w-4/12 mx-auto rounded-xl"
+        className="w-6/12 md:w-4/12 mx-auto rounded-md"
       />
-
-      <div className="pt-10 text-2xl font-bold">{temp} °C</div>
-      <div className="pt-4 text-green-600 font-bold text-4xl">{city}</div>
-      <div className="pt-4 text-2xl tracking-wide">{country}</div>
-
-      <div className="coord flex justify-center py-4 text-lg font-medium">
-        <div className="flex flex-col px-2">
-          <span className="font-bold">Latitude</span>
-          <span>{lat}</span>
+      <div className="flex flex-col justify-center items-center my-6">
+        <div className="py-4 text-green-600 font-bold text-2xl">
+          City : {city}
+        </div>
+        <div className=" text-xl font-semibold tracking-wide ">
+          Temparature : <span className="text-blue-600"> {temp} °C</span>
         </div>
 
-        <div className="flex flex-col px-2">
-          <span className="font-bold">Longitude</span>
-          <span>{lon}</span>
+        <div className="pt-4 text-xl tracking-wide font-semibold">
+          <span className="">Weather : </span>{" "}
+          <span className="text-blue-600">{weather}</span>
+        </div>
+        <div className="pt-4 text-xl tracking-wide font-semibold">
+          <span className=""> Description : </span>{" "}
+          <span className="text-blue-600">{weatherdes}</span>
+        </div>
+
+        <div className="pt-4 text-xl tracking-wide  text-green-600">
+          <span className="font-medium">Country Code : </span> {country}
+        </div>
+      </div>
+
+      <div className=" flex justify-around py-8 text-lg font-medium">
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold text-blue-600">Latitude</span>
+          <span>{lat}°</span>
+        </div>
+
+        <div className="flex flex-col gap-2">
+          <span className="font-semibold text-blue-600">Longitude</span>
+          <span>{lon}°</span>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center my-6">
+        <div className="pt-4 text-xl tracking-wide">
+          <span className="font-medium">Max Temparature : </span> {maxtemp} °C
+        </div>
+        <div className="pt-4 text-xl tracking-wide">
+          <span className="font-medium">Min Temparature : </span> {mintemp} °C
         </div>
       </div>
 
       <div className="flex justify-between py-4 text-lg font-medium mx-4 md:mx-10">
-        <div className="flex flex-col justify-between">
-          <IoWater className="text-blue-500 mx-auto my-2" fontSize={40} />
+        <div className="flex flex-col justify-between items-center">
+          <MdOutlineWaterDrop
+            className="text-blue-500 mx-auto my-2"
+            fontSize={40}
+          />
           <span>{humidity} %</span>
-          <span className="font-bold">Humidity</span>
+          <span className="font-semibold text-blue-600">Humidity</span>
         </div>
 
-        <div className="flex flex-col justify-between">
+        <div className="flex flex-col justify-between items-center">
           <WiCloudyWindy className="text-blue-500 mx-auto my-2" fontSize={50} />
           <span>{wind} Km/hr</span>
-          <span className="font-bold">Windspeed</span>
+          <span className="font-semibold text-blue-600">Windspeed</span>
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center items-center my-6">
+        <div className="pt-4 text-xl tracking-wide">
+          <span className="font-semibold">Atmospheric pressure : </span>
+          {pressure} hPa
+        </div>
+
+        <div className="pt-4 text-xl tracking-wide">
+          <span className="font-semibold">Ground level pressure : </span>
+          {grndlevel} hPa
+        </div>
+        <div className="pt-4 text-xl tracking-wide">
+          <span className="font-semibold">Sea level pressure : </span>{" "}
+          {sealevel} hPa
         </div>
       </div>
     </div>
